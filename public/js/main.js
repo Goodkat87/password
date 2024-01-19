d79865e64b5e816879404ff81bc487ad114e03c2
Goodkat87 <ilyass.zrl@hotmail.com>
2024-01-19 11:41:46 +0100

    Html and CSS

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+let copyText
+function copy() {
+    // Get the text field
+    copyText = document.getElementById("password");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  }
